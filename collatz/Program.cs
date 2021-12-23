6544a352785feb6eaf60df610a6aef959dfb83aa
David May <David.May@emishealth.com>
2021-12-23 13:37:25 +0000

    collatz - add longest chain calculation

--- collatz/Program.cs
+++ collatz/Program.cs
@@ -10,44 +10,125 @@ namespace Collatz
 
 		static int _numberToTest = 100000000;
 		static int _blockCount = 40;
-		static ulong _history_size = 83333333;
+		static ulong _history_size = 100000000;
 		static int[] _history;
+		static bool _calculateChain = true;
+		static long _maxChainSeed = 0;
+		static long _maxChainLength = 0;
+		static object _lockObject = new object();
 
 		static void Main(string[] args)
 		{
-			//arg 1: number to test (in millions)
-			//arg 2: number of parallel iterations
-			//arg 3: history_size (in millions)
+			//arg 1:	required: number to test (in millions)
+			//nc:		optional: do not calculate longest chain
+			//-b <int>: optional: number of parallel iterations
+			//-h <int>: optional: history_size (in millions)
 
 			Stopwatch stopWatch = new Stopwatch();
+			
 			stopWatch.Start();
-
-			if (args.Length > 0) _numberToTest = Int32.Parse(args[0]) * 1000000;
-			if (args.Length > 1) _blockCount = Int32.Parse(args[1]);
-			if (args.Length > 0) _history_size = (ulong)(Int32.Parse(args[2]) * 1000000);
+			
+			if (!TryParseArgs(args))
+			{
+				Console.WriteLine(@"Error parsing parameters.
+Arguments:
+arg1       = required: number to test in millions
+'nc'       = optional: do not calculate longest chain
+'-b <int>' = optional: number of parallel iterations (defaults to 40)
+'-h <int>' = optional: size of history array in millions (defaults to 100)");
+                
+				return;
+			}
 
 			_history = new int[_history_size];
 			int blockSize = _numberToTest / _blockCount;
 
 			bool success = TestCollatz(_blockCount, blockSize);
+
 			stopWatch.Stop();
 			TimeSpan tsOverall = stopWatch.Elapsed;
-
 			string runTime = String.Format("{0:00}:{1:00}:{2:00}.{3:00}", tsOverall.Hours, tsOverall.Minutes, tsOverall.Seconds, tsOverall.Milliseconds / 10);
 
-			Console.WriteLine($"NumberToTest: {_numberToTest}, HISTORY SIZE: {_history_size}, BLOCKCOUNT: {_blockCount}, BLOCKSIZE: {blockSize}");
-			Console.WriteLine($"RUN TIME: {runTime}");
-			Console.WriteLine(success ? "SUCCESS:" : "FAILURE");
+			Console.WriteLine(success ? "SUCCESS" : "FAILURE");
+			Console.WriteLine($"{"Number Under Test:", -25} {_numberToTest}");
+			Console.WriteLine($"{"Run Time:", -25} {runTime}");
+
+			if (_calculateChain)
+			{
+				Console.WriteLine($"{"Longest Chain Seed:", -25} {_maxChainSeed}");
+				Console.WriteLine($"{"Longest Chain Length:", -25} {_maxChainLength}");
+			}
+
+			// Console.WriteLine($"{"History Size:", -25} {_history_size}");
+			// Console.WriteLine($"{"Parallel Block Count:", -25} {_blockCount}");
+			// Console.WriteLine($"{"Block Size:", -25} {blockSize}");
 
 			Console.WriteLine("Press any key to exit ...");
 			Console.ReadLine();
+		}
+
+		private static bool TryParseArgs(string[] args)
+		{
+			if (args.Length == 0)
+				return false;
+
+			_numberToTest = Int32.Parse(args[0]) * 1000000;
+
+			int i = 1;
+
+			while (i < args.Length)
+			{
+				switch (args[i])
+				{
+					case "nc":
+						_calculateChain = false;
+						break;
+					case "-b":
+						_blockCount = Int32.Parse(args[i + 1]);
+						i++;
+						break;
+					case "-h":
+						_history_size = (ulong)(Int32.Parse(args[i + 1]) * 1000000);
+						i++;
+						break;
+					default:
+						Console.WriteLine($"Unknown argument {args[i]}");
+						return false;
+				}
+
+				i++;
+			}
+
+			return true;
 		}
 
 		private static bool TestCollatz(int blockCount, int blockSize)
 		{
 			try
 			{
-				Parallel.For(0, blockCount, b => TestCollatzBlock((b * blockSize) + 1, blockSize));
+				if (_calculateChain)
+				{
+					long[] maxChain = new long[_numberToTest + 1];
+
+					Parallel.For(
+						fromInclusive: 0,
+						toExclusive: blockCount,
+						body: (b) => TestCollatzBlockWithChainCalc((b * blockSize) + 1, blockSize, maxChain));
+
+					for (long lng = 1; lng <= _numberToTest; lng++)
+					{
+						if (maxChain[lng] > _maxChainLength)
+						{
+							_maxChainLength = maxChain[lng];
+							_maxChainSeed = lng;
+						}
+					}
+				}
+				else
+					Parallel.For(
+						fromInclusive: 0,
+						toExclusive: blockCount,
+						body: (b) => TestCollatzBlockNoChainCalc((b * blockSize) + 1, blockSize));
 			}
 			catch (AggregateException ex)
 			{
@@ -62,15 +143,51 @@ namespace Collatz
 			return true;
 		}
 
-		private static void TestCollatzBlock(int start, int blockSize)
+		private static void TestCollatzBlockWithChainCalc(int start, int blockSize, long[] maxChain)
 		{
-			int r;
-			ulong itr;
+			ulong itr = 1;
+			long chainLength = 0;
 			int historyNum;
 
-			for (r = start; r < start + blockSize; r++)
+			for (int seed = start; seed < start + blockSize; seed++)
 			{
-				itr = (ulong)r;
+				chainLength = 0;
+				itr = (ulong)seed;
+
+				// loop while not a power of 2
+				while ((itr & (itr - 1)) != 0)
+				{
+					if (itr < _history_size)
+					{
+						historyNum = _history[itr];
+
+						if (historyNum == 0)
+							_history[itr] = seed; // we have not met this number before
+						else if (historyNum > seed)
+							_history[itr] = seed; // this number already found in a chain with a higher seed value. Follow to the end of the chain here as we are the lower seed value
+						else if (historyNum == seed)
+							throw new InvalidDataException($"FAILURE ({seed})"); // we have dropped into a loop
+					}
+
+					chainLength++;
+
+					itr = (itr & 1) == 0
+						? itr = itr / 2
+						: itr = (itr * 3) + 1;
+				}
+
+				maxChain[seed] = chainLength + WhatPowerOf2(itr);
+			}
+		}
+
+		private static void TestCollatzBlockNoChainCalc(int start, int blockSize)
+		{
+			ulong itr = 1;
+			int historyNum;
+
+			for (int seed = start; seed < start + blockSize; seed++)
+			{
+				itr = (ulong)seed;
 
 				// loop while not a power of 2
 				while ((itr & (itr - 1)) != 0)
@@ -81,26 +198,35 @@ namespace Collatz
 					{
 						historyNum = _history[itr];
 
-						if (historyNum == 0 || historyNum > r)
-						{
-							// historyNum == 0 : we have not met this number before
-							// historyNum > r  : we have met this number before in a chain with seed > r. Let the lower seeded chain follow the chain to the end
-							_history[itr] = r;
-							goto NextIteration;
-						}
+						if (historyNum == 0)
+							_history[itr] = seed; // we have not met this number before
+						else if (historyNum > seed)
+							_history[itr] = seed; // this number already found in a chain with a higher seed value. Follow to the end of the chain here as we are the lower seed value
+						else if (historyNum == seed)
+							throw new InvalidDataException($"FAILURE ({seed})"); // we have dropped into a loop
+						else
+							break; // this number already found in a chain with a lower seed value. No need to follow to the end as we are not calculating chain length
 
-						if (historyNum < r)
-							break; // found in another chain. Let the lower seeded chain follow the chain to the end
-
-						throw new InvalidDataException($"FAILURE ({r})"); // historyNum == r means already found in this iteration so this is a failure
 					}
 
-				NextIteration:
 					itr = (itr & 1) == 0
 						? itr = itr / 2
 						: itr = (itr * 3) + 1;
 				}
 			}
+		}
+
+		private static long WhatPowerOf2(ulong num)
+		{
+			long exponent = 0;
+
+			while (num != 1)
+			{
+				num = num >> 1;
+				exponent++;
+			}
+
+			return exponent;
 		}
 	}
 }
