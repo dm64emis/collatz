535851c4df063791bcfaf658b0d721d0cc382fe4
David May <David.May@emishealth.com>
2021-12-24 10:22:22 +0000

    collatz - add history array to max chain calc

--- collatz/Program.cs
+++ collatz/Program.cs
@@ -17,47 +17,27 @@ namespace Collatz
 		static void Main(string[] args)
 		{
 			Stopwatch stopWatch = new Stopwatch();
-			
+
 			stopWatch.Start();
-			
+
 			if (!TryParseArgs(args))
 			{
 				Console.WriteLine(@"Error parsing parameters.
 Arguments:
-arg1        = required: number to test in millions
-'-xc <int>' = optional: do not calculate longest chain. This paramater has an optional <int> to set the size of the history array in millions (defaults to 40)");
-                
+arg1       = required: number to test in millions (defaults to 100 million)
+'-x'       = optional: do not calculate longest chain.
+'-h <int>  = optional: set the size of the history array in millions (defaults to 40)");
+
 				return;
 			}
 
 			if (_calculateChain)
-			{
-				TestCollatz_c0();
-			}
+				TestCollatz_c1();
 			else
-			{
 				TestCollatz_xc0();
-			}
 
 			stopWatch.Stop();
-			TimeSpan tsOverall = stopWatch.Elapsed;
-			string runTime = String.Format("{0:00}:{1:00}:{2:00}.{3:00}", tsOverall.Hours, tsOverall.Minutes, tsOverall.Seconds, tsOverall.Milliseconds / 10);
-
-			Console.WriteLine($"{"Number Under Test:", -25} {_numberToTest}");
-			Console.WriteLine($"{"Run Time:", -25} {runTime}");
-
-			Console.WriteLine($"{"Calculate Chain:", -25} {_calculateChain}");
-
-			if (_calculateChain)
-			{
-				Console.WriteLine($"{"Longest Chain Seed:", -25} {_maxChainSeed}");
-				Console.WriteLine($"{"Longest Chain Length:", -25} {_maxChainLength}");
-			}
-
-			Console.WriteLine($"{"History Size:", -25} {_history_size}");
-
-			Console.WriteLine("Press any key to exit ...");
-			Console.ReadLine();
+			ReportSummary(stopWatch.Elapsed);
 		}
 
 		private static bool TryParseArgs(string[] args)
@@ -73,20 +53,18 @@ arg1        = required: number to test in millions
 			{
 				switch (args[i])
 				{
-					case "-xc":
+					case "-x":
 						_calculateChain = false;
+						break;
+					case "-h":
 						i++;
-
-						if (args.Length > i)
-						{
-							_history_size = Int32.Parse(args[i]) * 1000000;
-							i++;
-						}
-
+						_history_size = Int32.Parse(args[i]) * 1000000;
 						break;
 					default:
 						return false;
 				}
+
+				i++;
 			}
 
 			return true;
@@ -111,7 +89,7 @@ arg1        = required: number to test in millions
 							// historyNum == 0:   we have not met this number before
 							// historyNum > seed: this number already found in a chain with a higher seed value. Follow to the end of the chain here as we are the lower seed value
 							// note: no need to lock the array. Clashes will resolve themselves in later iterations
-							history[itr] = seed; 
+							history[itr] = seed;
 						}
 						else if (historyNum < seed)
 							break; // this number already found in a chain with a lower seed value. No need to follow to the end as we are not calculating chain length
@@ -130,7 +108,7 @@ arg1        = required: number to test in millions
 		{
 			Object lockObject = new Object();
 
-			// calculate the longest chain. History array did not provide any benefit here.
+			// calculate the longest chain. Without history array.
 			Parallel.For(1, _numberToTest + 1, seed => {
 				long chainLength = 1;
 				long itr = seed;
@@ -154,7 +132,42 @@ arg1        = required: number to test in millions
 			});
 		}
 
-		private void TestCollatz_history_size(Stopwatch stopWatch)
+		private static void TestCollatz_c1()
+		{
+			// calculate the longest chain. Uses history array.
+			Object lockObject = new Object();
+			long[] history = new long[_history_size];
+
+			Parallel.For(1, _numberToTest + 1, seed => {
+				long itr = seed;
+				long chainLength = 1;
+
+				while (itr > 1)
+				{
+					if (itr < _history_size)
+					{
+						if (chainLength <= history[itr])
+							return; // a longer chain already passed through this number
+
+						history[itr] = chainLength; // locking not necessary. If the update fails due to a clash, it means the other seed loops may have more iterations than optimal. Adding a lock slows it down considerably
+					}
+
+					itr = (itr & 1) == 0
+						? itr = itr / 2
+						: itr = (itr * 3) + 1;
+					chainLength++;
+				}
+
+				if (chainLength > _maxChainLength)
+					lock (lockObject)
+					{
+						_maxChainSeed = seed;
+						_maxChainLength = chainLength;
+					}
+			});
+		}
+
+		private static void TestCollatz_history_size(Stopwatch stopWatch)
 		{
 			TimeSpan tsMin = new TimeSpan(0);
 			int hMin = 0;
@@ -165,7 +178,7 @@ arg1        = required: number to test in millions
 				_history_size = h * 1000000;
 				stopWatch.Reset();
 				stopWatch.Start();
-				TestCollatz_xc0();
+				TestCollatz_c1();
 				stopWatch.Stop();
 				TimeSpan ts = stopWatch.Elapsed;
 
@@ -181,6 +194,27 @@ arg1        = required: number to test in millions
 
 			runTime = String.Format("{0:00}.{1:00}", tsMin.Seconds, tsMin.Milliseconds / 10);
 			Console.WriteLine($"Min Run Time: {runTime}, h: {hMin}");
+		}
+
+		private static void ReportSummary(TimeSpan ts)
+		{
+			string runTime = String.Format("{0:00}:{1:00}:{2:00}.{3:00}", ts.Hours, ts.Minutes, ts.Seconds, ts.Milliseconds / 10);
+
+			Console.WriteLine($"{"Number Under Test:", -25} {_numberToTest}");
+			Console.WriteLine($"{"Run Time:", -25} {runTime}");
+
+			Console.WriteLine($"{"Calculate Chain:", -25} {_calculateChain}");
+
+			if (_calculateChain)
+			{
+				Console.WriteLine($"{"Longest Chain Seed:", -25} {_maxChainSeed}");
+				Console.WriteLine($"{"Longest Chain Length:", -25} {_maxChainLength}");
+			}
+
+			Console.WriteLine($"{"History Size:", -25} {_history_size}");
+
+			Console.WriteLine("Press any key to exit ...");
+			Console.ReadLine();
 		}
 	}
 }
